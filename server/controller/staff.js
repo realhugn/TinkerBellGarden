bbb05a6f647808b085cebc5d7f263ecc2e12c66e
realhugn <81417323+realhugn@users.noreply.github.com>
2023-01-14 22:12:17 +0700

    add staff route (not tested yet)

--- server/controller/staff.js
+++ server/controller/staff.js
@@ -17,9 +17,86 @@ export const getAllStaffs = async (req, res, next) => {
 export const getStaff = async (req, res, next) => {
     try {
         const staff = await Staff.findById(req.user._id).select('-password');
-        if (!staff) return res.status(404).json({ msg: "No customer found" });
+        if (!staff) return res.status(404).json({ msg: "No Staff found" });
         return res.status(200).json({data: staff})
     } catch (error) {
+        return res.json({
+            message: "Interval Server Error",
+            error,
+        })
+    }
+}
+
+export const changePassword = async (req, res, next) => {
+    try {
+        const _id = req.params.id
+
+        if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send({ message: 'invalid id' })
+
+        const { oldPassword, newPassword, confirmNewPassword } = req.body
+
+        if (!oldPassword || !newPassword || !confirmNewPassword)
+            return res.status(400).json({ success: false, msg: 'Please fill all the field' })
+        if (newPassword !== confirmNewPassword)
+            return res.status(400).json({ success: false, msg: "New password and confirm password do not match" })
+        const staff = await Staff.findById(_id)
+        if (! await staff.isValidPassword(oldPassword)) return res.status(400).json({success: false, msg: "Wrong old password"})
+        const updatePassword = await bcrypt.hash(newPassword, 12)
+        const updatedStaff = await Staff.findByIdAndUpdate(_id, {password: updatePassword})
+        return res.status(200).json({success: true,msg: 'Update new password successfully', data : updatedStaff})
+    } catch (error) {
+        console.log(error);
+        return res.json({
+            message: "Interval Server Error",
+            error,
+        })
+    }
+}
+
+export const updateStaff = async (req, res, next) => {
+    try {
+        let params = {
+            name: req.body.name,
+            phone: req.body.phone,
+            address: req.body.address
+        }
+        for (let prop in params) if (!params[prop]) delete params[prop]
+        if (!mongoose.Types.ObjectId.isValid(req.params.id)) return res.status(404).json({success:false, msg: 'invalid id'})
+        const newData = await Staff.findByIdAndUpdate(req.params.id, params, { new: true }).select('-password')
+         if (!newData)
+            return res.json({
+                success: false,
+                msg:"No Staff found"
+            }) 
+        return res.status(200).json({
+            success: true,
+            msg: "Updated Successfully",
+            data: newData
+        })
+    } catch (error) {
+        return res.json({
+            message: "Interval Server Error",
+            error,
+        })
+    }
+}
+
+export const deleteStaff = async (req, res, next) => {
+    try {
+        const _id = req.params.id
+        if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send({ message: 'invalid id' })
+        const data = await Staff.findById(_id);
+        if (data)
+        {
+            await Staff.findByIdAndUpdate(_id,{status: false},{new: true})                  
+            return res.status(200).json({ msg: "Delete Succesfully"})}
+        else
+            return res.json({
+                success: false,
+                msg:"No user found"
+            }) 
+    } catch (error) {
+        console.log(error);
         return res.json({
             message: "Interval Server Error",
             error,
--- server/routes/staffRoute.js
+++ server/routes/staffRoute.js
@@ -1,8 +1,12 @@
 import express from 'express'
 import { isAuth, isManager, verifyToken } from '../middlewares/auth.js'
-import { getAllStaffs } from '../controller/staff.js'
+import { deleteStaff, getAllStaffs, getStaff, updateStaff } from '../controller/staff.js'
 const router = express.Router()
 
 router.get('/', verifyToken, isManager, getAllStaffs) // only manager can get all staffs
+router.get('/:id', verifyToken, isAuth, getStaff)
+router.put('/:id', verifyToken, isAuth, updateStaff) 
+router.put('/:id/change-password', verifyToken, isAuth, changePassword)
+router.delete('/:id', verifyToken, isAuth, deleteStaff) 
 
 export default router
